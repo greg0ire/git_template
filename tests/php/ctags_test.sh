d62e5e22ce9385e2b7c73939fed4762139ebffd5
Grégoire Paris <postmaster@greg0ire.fr>
2013-09-12 00:29:23 +0200

    remove debugging

--- tests/php/ctags_test.sh
+++ tests/php/ctags_test.sh
@@ -20,10 +20,6 @@ testTagsFileWorksWithSymfony1()
 	assertTrue 'The tags file was not generated' "[ -f .git/tags ]"
 	assertTrue "\$indexMe was not found here : `cat .git/tags`" "grep indexMe .git/tags"
 	assertFalse '$doNotIndexMe was found' "grep doNotIndexMe .git/tags"
-	bash -eux .git/hooks/php/ctags/update-ctags
-	ls -R
-	ctags -h .php --verbose -Rf/tmp/tags --languages=php --totals=yes --tag-relative=yes --PHP-kinds=cfiv '--exclude=*.js' --exclude=.svn --exclude=.git '--exclude=vendor/*/vendor' '--exclude=vendor/*/Tests' --fields=+aimS '--exclude=cache/*'
-	cat /tmp/tags
 }
 
 testTagsFileWorksWithSymfony2()
