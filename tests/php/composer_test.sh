39aa7aa7275017d5c9ded7264ff15910a8a554d6
Grégoire Paris <postmaster@greg0ire.fr>
2013-11-03 14:59:33 +0100

    fixes #13
    the post-merge hook was not working at all!

--- tests/php/composer_test.sh
+++ tests/php/composer_test.sh
@@ -45,6 +45,35 @@ testRunsComposerOnComposerLockCheckoutChange()
 	assertTrue 'Composer was not run' "[ `cat ${SHUNIT_TMPDIR}/composerWasRun` == "install" ]"
 }
 
+testRunsComposerOnPostMerge()
+{
+	composer()
+	{
+		echo $1 > "${SHUNIT_TMPDIR}/composerWasRun"
+	}
+	export -f composer
+	export SHUNIT_TMPDIR
+	initRepo
+	echo "" > "${SHUNIT_TMPDIR}/composerWasRun"
+	echo "a" > composer.lock
+	git add composer.lock
+	git commit -qm "first version of composer.lock"
+	cd ..
+	git clone -q test_repo test_repo2
+	cd test_repo2
+	git config hooks.enabled-plugins php/composer
+	touch someFile
+	git add someFile
+	git commit -qm "someFile"
+	cd ../test_repo
+	echo "b" > composer.lock
+	git add composer.lock
+	git commit -qm "second version of composer.lock"
+	cd - > /dev/null
+	git pull -q --no-edit > /dev/null 2>&1
+	assertTrue 'Composer was not run' "[ `cat ${SHUNIT_TMPDIR}/composerWasRun` == "install" ]"
+}
+
 initRepo()
 {
 	cd $testRepo
