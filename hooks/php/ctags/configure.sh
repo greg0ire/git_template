8e175a04f62dcf33839f5a4c88bd89f91ae164ef
Grégoire Paris <postmaster@greg0ire.fr>
2014-02-20 20:01:38 +0100

    Make the project type configurable

--- hooks/php/ctags/configure.sh
+++ hooks/php/ctags/configure.sh
@@ -2,8 +2,28 @@
 . .git/hooks/hook_switcher.sh
 
 main() {
+	local projectType
 	echo "Configuring php/ctags"
 	switch_hook php/ctags
+
+
+	if hook_is_enabled php/ctags
+	then
+		PS3="Select a project type : "
+		select projectType in symfony1 symfony2 none;
+		do
+			case $projectType in
+				symfony1|symfony2)
+					git config hooks.php-ctags.project-type $projectType
+					break
+				;;
+				none)
+					echo "no project type... do nothing"
+					break
+				;;
+			esac
+		done
+	fi
 }
 
 main
