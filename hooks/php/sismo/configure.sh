78b16bfabd1ff862a074eccbd2d4795af1e21e44
Grégoire Paris <postmaster@greg0ire.fr>
2014-04-01 21:33:36 +0200

    configuration script for sismo

---
+++ hooks/php/sismo/configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+. .git/hooks/hook_switcher.sh
+
+main() {
+	if hook_is_enabled php/sismo
+	then
+		read -p "Specify the sismo slug for this project : "
+		git config hooks.php-sismo.slug $REPLY
+	fi
+}
+
+main
