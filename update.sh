fbb3e6a54dbc4130d4c2e3b3977687d22749ef9a
Grégoire Paris <gparis@universcine.com>
2014-04-22 12:34:11 +0200

    use templateDir instead of PROGDIR
    it changes less, and should never change

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eu
-readonly PROGDIR=$(readlink --canonicalize-missing $(dirname $0))
 main()
 {
 	local templateDir=$(git config --get --path init.templatedir)
+
 	if [ ! -d .git ]
 	then
 		echo "This script is supposed to be run at the root of a git repository" >&2
 	fi
-	rsync --archive --verbose --compress --cvs-exclude $PROGDIR/hooks/ .git/hooks --delete
-	rsync --archive --verbose --compress --cvs-exclude --exclude="update.sh" --exclude="tests" $PROGDIR/ .git
+	rsync --archive --verbose --compress --cvs-exclude $templateDir/hooks/ .git/hooks --delete
+	rsync --archive --verbose --compress --cvs-exclude --exclude="update.sh" --exclude="tests" $templateDir/ .git
 }
 main
