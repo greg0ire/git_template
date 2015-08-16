9cf44cec5da6527072b4564f2d18528060578421
Grégoire Paris <postmaster@greg0ire.fr>
2015-08-17 00:08:55 +0200

    refactor file change detection

---
+++ template/hooks/change_detector.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# @param hook_type       post-checkout|post-merge|pre-commit
+# @param monitored_paths the paths to check
+has_changed()
+{
+	local hook_type=$1; shift
+	local monitored_paths=($@)
+	local against
+	local changed
+
+	monitored_paths=( "${monitored_paths[@]/#/$GIT_DIR/../}" )
+
+	case $hook_type in
+		post-checkout | post-merge)
+			changed="$(git diff 'HEAD@{1}' \
+				--stat \
+				-- ${monitored_paths[*]}| wc --lines)"
+			;;
+		pre-commit)
+				if git rev-parse --verify HEAD >/dev/null 2>&1
+				then
+					against=HEAD
+				else
+					# Initial commit: diff against an empty tree object
+					against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+				fi
+				changed="$(git diff-index \
+					--name-status $against \
+					-- composer.json | wc --lines)"
+			;;
+	esac
+
+	if [ "$changed" -gt 0 ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
