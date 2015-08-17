df276323ae78ac4c7fcc99666b701e22c19ab734
Grégoire Paris <postmaster@greg0ire.fr>
2015-08-17 23:56:42 +0200

    run doctrine hook only when relevant

--- template/hooks/change_detector.sh
+++ template/hooks/change_detector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# @param hook_type       post-checkout|post-merge|pre-commit
+# @param hook_type       post-checkout|post-merge|post-commit|pre-commit
 # @param monitored_paths the paths to check
 has_changed()
 {
@@ -12,7 +12,19 @@ has_changed()
 	monitored_paths=( "${monitored_paths[@]/#/$GIT_DIR/../}" )
 
 	case $hook_type in
-		post-checkout | post-merge)
+		post-commit)
+			if git rev-parse --verify HEAD^ >/dev/null 2>&1
+			then
+				against=HEAD^
+			else
+				# Initial commit: diff against an empty tree object
+				against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+			fi
+			changed="$(git diff-tree 'HEAD' $against \
+				--stat \
+				-- ${monitored_paths[*]}| wc --lines)"
+			;;
+		post-checkout | post-merge )
 			changed="$(git diff 'HEAD@{1}' \
 				--stat \
 				-- ${monitored_paths[*]}| wc --lines)"
--- tests/php/doctrine_test.sh
+++ tests/php/doctrine_test.sh
@@ -3,49 +3,52 @@ testTriggeredOnValidateReturns2()
 {
 	console()
 	{
-		echo "$1" > "${SHUNIT_TMPDIR}/consoleWasRun"
+		echo "$1" > "${SHUNIT_TMPDIR}/consoleArgs"
 		return 2
 	}
 	read()
 	{
 		REPLY=y
 	}
+	touch "${SHUNIT_TMPDIR}/consoleArgs"
 	export -f console
 	export -f read
 	export SHUNIT_TMPDIR
 	initRepo
-	echo "a" > someFile
-	git add someFile
-	git commit --quiet --message "let's commit someFile" > /dev/null 2>&1
+	echo "a" > composer.lock
+	git add composer.lock
+	git commit --quiet --message "let's commit composer.lock" > /dev/null 2>&1
 	assertEquals 'Update was not run' \
 		"doctrine:schema:update" \
-		"$(cat "${SHUNIT_TMPDIR}/consoleWasRun")"
+		"$(cat "${SHUNIT_TMPDIR}/consoleArgs")"
 
 	mkdir --parents vendor/doctrine/migrations
-	echo "b" > someFile
-	git add someFile
-	git commit --quiet --message "let's commit someFile" > /dev/null 2>&1
+	echo "b" > composer.lock
+	git add composer.lock
+	git commit --quiet --message "let's commit composer.lock" > /dev/null 2>&1
 	assertEquals 'Migrate was not run' \
 		"doctrine:migrations:migrate" \
-		"$(cat "${SHUNIT_TMPDIR}/consoleWasRun")"
+		"$(cat "${SHUNIT_TMPDIR}/consoleArgs")"
 }
 
 testNoChangesWhenInvalidSchema()
 {
 	console()
 	{
-		echo "$1" > "${SHUNIT_TMPDIR}/consoleWasRun"
+		echo "$1" > "${SHUNIT_TMPDIR}/consoleArgs"
 		return 1
 	}
+	touch "${SHUNIT_TMPDIR}/consoleArgs"
 	export -f console
 	export SHUNIT_TMPDIR
 	initRepo
-	echo "a" > someFile
-	git add someFile
+	mkdir --parents src/AppBundle/Entity
+	echo "a" > src/AppBundle/Entity/someFile
+	git add .
 	git commit --quiet --message "let's commit someFile" > /dev/null 2>&1
 	assertEquals 'Only validate was run' \
 		"doctrine:schema:validate" \
-		"$(cat "${SHUNIT_TMPDIR}/consoleWasRun")"
+		"$(cat "${SHUNIT_TMPDIR}/consoleArgs")"
 }
 
 initRepo()
